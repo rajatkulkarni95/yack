18825d158b8f5d31a9c9f833ddb30cdcc11960d5
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-04-15 15:47:37 +0530

    Tailwind, menubar, shortcuts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./index.css";
+
+import { register } from "@tauri-apps/api/globalShortcut";
+
+register("Command+Shift+Space", async () => {
+  const appWindow = await (await import("@tauri-apps/api/window")).appWindow;
+
+  await appWindow.show();
+  await appWindow.setFocus();
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>

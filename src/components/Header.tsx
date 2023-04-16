68732dbf3ffe1301ed32fa265fb50ce3941ca11f
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-04-16 22:44:50 +0530

    Icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import KbdShort from "./KbdShort";
 import { useUsage } from "../hooks/useUsage";
 import { useGlobalShortcut } from "../hooks/useGlobalShortcut";
+import { PaintBrushIcon } from "@heroicons/react/24/solid";
 
 interface IHeaderProps {
   haltNew?: boolean;
@@ -26,7 +27,7 @@ const Header = ({ haltNew, hideHistory }: IHeaderProps) => {
   return (
     <header className="p-4 flex bg-secondary border-b border-primary h-12">
       <div className="flex items-center justify-between w-full">
-        <h1 className="text-xl font-bold text-primary">NameTBD</h1>
+        <PaintBrushIcon className="w-6 h-6 text-primary" />
         <div className="flex items-center">
           <div className="px-2 py-1 bg-transparent cursor-default select-none hover:bg-primaryBtnHover font-mono text-secondary rounded mr-4">
             ${totalCost}

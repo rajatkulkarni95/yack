c08968636a66c245741f193569a006a95af69dd1
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-04-17 20:22:25 +0530

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import KbdShort from "./KbdShort";
 import { useUsage } from "../hooks/useUsage";
 import { useGlobalShortcut } from "../hooks/useGlobalShortcut";
 import { PaintBrushIcon } from "@heroicons/react/24/solid";
+import { LogoIcon } from "../svg";
 
 interface IHeaderProps {
   haltNew?: boolean;
@@ -27,7 +28,7 @@ const Header = ({ haltNew, hideHistory }: IHeaderProps) => {
   return (
     <header className="p-4 flex bg-secondary border-b border-primary h-12">
       <div className="flex items-center justify-between w-full">
-        <PaintBrushIcon className="w-6 h-6 text-primary" />
+        <LogoIcon className="w-6 h-6 text-primary" />
         <div className="flex items-center">
           <div className="px-2 py-1 bg-transparent cursor-default select-none hover:bg-primaryBtnHover font-mono text-secondary rounded mr-4">
             ${totalCost}

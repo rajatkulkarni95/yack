eb2adc5f20d62b66e3fc75abcdb1de2aeae1bab6
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-04-22 11:19:47 +0530

    Fix header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useEffect } from "react";
 import TokenRegistration from "../components/TokenRegistration";
 import { useNavigate } from "react-router-dom";
+import { LogoIcon } from "../svg";
 
 function Index() {
   const navigate = useNavigate();
@@ -15,8 +16,10 @@ function Index() {
 
   return (
     <Fragment>
-      <header className="p-4 flex bg-secondary border-b border-primary flex-col min-h-[64px]">
-        <h1 className="text-2xl font-bold text-primary mb-1 uppercase">Yack</h1>
+      <header className="px-4 py-2 flex bg-secondary border-b border-primary flex-col ">
+        <div className="flex items-center">
+          <LogoIcon className="w-20 h-10 text-primary" />{" "}
+        </div>
         <p className="text-base font-normal text-secondary">
           Instantly access ChatGPT on your Mac - get quick answers to anything
           with just a few taps on your keyboard!

228e9c5b303c1c319a4c323d5e1415a158f18263
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-05-07 22:08:17 +0530

    Insert div and lang

--- src/components/Chat/Bubble.tsx
+++ src/components/Chat/Bubble.tsx
@@ -10,9 +10,8 @@ type TChatBubble = {
 };
 
 function extractLanguageFromCodeBlock(inputString: string): string | null {
-  const codeBlockRegex = /^```([\w+#-]+)$/gm;
-  const match = codeBlockRegex.exec(inputString);
-  return match ? match[1] : "javascript";
+  const splitString = inputString.split(" ");
+  return splitString[0] || "Unknown";
 }
 
 const ChatBubble = ({ message, loading }: TChatBubble) => {
@@ -31,24 +30,22 @@ const ChatBubble = ({ message, loading }: TChatBubble) => {
 
   // Function that finds all pre > code blocks in the dom, and adds a button with a copy icon to each
   // which copies the code to the clipboard when clicked
-  const addCopyButtons = () => {
-    const codeBlocks = document.querySelectorAll("pre > code");
-    codeBlocks.forEach((codeBlock) => {
-      if (codeBlock.parentElement?.querySelector(".copy-button")) return;
-
-      const copyButton = document.createElement("button");
-      copyButton.className =
-        "copy-button absolute top-1 right-1 flex z-10 p-1 bg-secondary rounded bg-transparent hover:bg-tertiary group opacity-0 transition-opacity duration-200";
-      copyButton.style.cursor = "pointer";
-      copyButton.innerHTML = `
+  const addCopyButtons = (codeBlock: Element) => {
+    const copyButton = document.createElement("button");
+    copyButton.className =
+      "copy-button flex z-10 p-1 bg-secondary rounded bg-transparent hover:bg-tertiary group transition-opacity duration-200";
+    copyButton.style.cursor = "pointer";
+    copyButton.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-icon group-hover:text-primary">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184" />
       </svg>
     `;
-      copyButton.addEventListener("click", () => copyCode(codeBlock));
 
-      codeBlock.parentNode?.appendChild(copyButton);
+    copyButton.addEventListener("click", () => {
+      copyCode(codeBlock);
     });
+
+    return copyButton;
   };
 
   const copyCode = (codeBlock: Element) => {
@@ -64,7 +61,21 @@ const ChatBubble = ({ message, loading }: TChatBubble) => {
   };
 
   useEffect(() => {
-    addCopyButtons();
+    const codeBlocks = document.querySelectorAll("pre > code");
+    codeBlocks.forEach((codeBlock) => {
+      if (codeBlock.parentElement?.querySelector(".copy-button")) return;
+      const lang = extractLanguageFromCodeBlock(
+        codeBlock.parentElement?.className || ""
+      );
+      const langDiv = document.createElement("div");
+      langDiv.className =
+        "text-xs w-full text-secondary bg-secondary box-border rounded-t px-3 py-1 flex items-center justify-between";
+      langDiv.innerHTML = `<span>${lang}</span>`;
+
+      langDiv.appendChild(addCopyButtons(codeBlock));
+
+      codeBlock.parentNode?.appendChild(langDiv);
+    });
   }, [html]);
 
   const anchorRegex = /(https:\/\/\S+)/g;

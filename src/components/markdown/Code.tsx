53646c188d79fa8a23407e4b109c37869d27bbf2
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-05-20 19:25:03 +0530

    Add copied and check icon for 5 secs post success copy

--- src/components/markdown/Code.tsx
+++ src/components/markdown/Code.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { CopyIcon } from "../../svg";
+import React, { useState } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { CheckIcon, CopyIcon } from "../../svg";
 
 type TProps = {
   inline?: boolean;
@@ -8,35 +10,43 @@ type TProps = {
 };
 
 const MarkdownCode = ({ inline, className, children, ...props }: TProps) => {
+  const [isCopied, setIsCopied] = useState(false);
   const copyCode = (codeBlock: string) => {
     navigator.clipboard.writeText(codeBlock).then(
       () => {
-        console.log("Copied to clipboard");
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 5000);
       },
       (err) => {
         console.error("Could not copy text: ", err);
       }
     );
   };
-
   const match = /language-(\w+)/.exec(className || "");
   return !inline && match ? (
     <React.Fragment>
-      <div className="box-border flex w-full items-center justify-between overflow-y-hidden rounded-t-md border-b border-primary bg-hover px-3 py-1 text-xs text-secondary">
+      <div className="box-border flex items-center justify-between overflow-y-hidden rounded-t-md border-b border-primary bg-hover px-3 py-1 text-xs text-secondary">
         <span>{match[1]}</span>
-        <button
-          className="group flex rounded bg-transparent p-1 transition-opacity duration-200 hover:bg-tertiary"
-          onClick={() => copyCode(String(children).replace(/\n$/, ""))}
-        >
-          <CopyIcon className="h-4 w-4 text-icon group-hover:text-primary" />
-        </button>
+        {isCopied ? (
+          <span className="flex items-center p-1 text-xs">
+            Copied <CheckIcon className="ml-1 h-4 w-4 text-green-600" />
+          </span>
+        ) : (
+          <button
+            className="group flex rounded bg-transparent p-1 transition-opacity duration-200 hover:bg-tertiary"
+            onClick={() => copyCode(String(children).replace(/\n$/, ""))}
+          >
+            <CopyIcon className="h-4 w-4 text-icon group-hover:text-primary" />
+          </button>
+        )}
       </div>
-      <code
-        className={`${match[1] === "bash" ? "language-sh" : match[1]}`}
-        {...props}
+      <SyntaxHighlighter
+        language={match[1]}
+        style={nightOwl}
+        PreTag={({ children }) => <React.Fragment>{children}</React.Fragment>}
       >
         {String(children).replace(/\n$/, "")}
-      </code>{" "}
+      </SyntaxHighlighter>
     </React.Fragment>
   ) : (
     <code className={className} {...props}>
